c697b87d93b26d56ab305e04b1dc68c3cce6913f
Ole Spaarmann <ole.spaarmann@factoryberlin.com>
2017-08-02 17:22:48 +0200

    Solutions for SVM and Naive Bayes

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,7 +24,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+import numpy as np
+from sklearn.metrics import accuracy_score
+t0 = time()
+clf = SVC(kernel='rbf', C=10000.0)
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+accuracy = accuracy_score(labels_test, pred)
+print(accuracy)
 
 #########################################################
-
-
